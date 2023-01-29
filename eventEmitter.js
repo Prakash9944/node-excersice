8bf83691d58a35177c0d23e9f144f2309be3a656
Prakash <prakash@atatus.com>
2023-01-29 11:15:11 +0530

    Added event emitter

--- eventEmitter.js
+++ eventEmitter.js
@@ -1,16 +1,29 @@
-const eventEmitter = require('events');
-const myEmitter = new eventEmitter();
 
-myEmitter.on('prakashraj', function () {
-    console.log("1 the event fired")
-})
+// get the reference of EventEmitter class of events module
+const event = require('events');
 
-myEmitter.on('prakashraj', function () {
-    console.log("2 the event fired")
-})
+//create an object of EventEmitter class by using above reference
+const eventEmitter = new event();
 
-myEmitter.on('prakashraj', function (data) {
-    console.log("3 the event fired", data)
-})
+// eventEmitter.on('prakashraj', function () {
+//     console.log("1 the event fired")
+// })
 
-myEmitter.emit('prakashraj');
+// eventEmitter.on('prakashraj', function () {
+//     console.log("2 the event fired")
+// })
+
+// eventEmitter.on('prakashraj', function (data) {
+//     console.log("3 the event fired", data)
+// })
+
+// eventEmitter.emit('prakashraj');
+
+
+// Subscribe for FirstEvent
+eventEmitter.on('FirstEvent', function (data) {
+    console.log('First subscriber: ' + data);
+});
+
+// Raising FirstEvent
+eventEmitter.emit('FirstEvent', 'This is my first Node.js event emitter example.');
