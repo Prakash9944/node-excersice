3bbd5d7fc3dbb6d645158fb320e6f83a76b9aeea
Prakash <prakash@atatus.com>
2024-07-24 19:40:00 +0530

    Update arrow function

--- arrowfunction.js
+++ arrowfunction.js
@@ -1,46 +1,50 @@
-// https://www.youtube.com/watch?v=ONdoSoFDvXI
+/* https://www.youtube.com/watch?v=ONdoSoFDvXI */
 
+// 1. Arrow functions offer a shorter syntax and lexical scoping of this
+// 2. Arrow functions do not have their own this context. Instead, they inherit this from the surrounding lexical context
+// 3. Arrow function does not have new keyword its not construct
+// 4. In an arrow function, the this keyword is lexically bound, meaning it refers to the this value of the surrounding (parent) context in which the arrow  function is defined.
 
-// const obj = {
-//     simple() {
-//         return this
-//     }
-// }
+var obj = {
+    simple() {
+        return this
+    }
+}
 
-// let a = obj.simple
-// console.log(obj.simple() === global)
-// console.log(a(), global)
+var extractFun = obj.simple;
+// console.log("Compare two funciton type: ", obj.simple() === global) // False
+// console.log("Compare two funciton type: ", extractFun() === global) // True
 
-// console.log(x) //  var keyword declaration are hosted initialization not
-// var x = 100
+console.log("Here var keyword are hoisted not initialization: ", number) // var keyword declaration are hosted initialization not
+var number = 100
 
-// console.log(x) //  const keyword not hosted
-// const x = 100
+// console.log("Here const keyword not hoisted because const did not have default value: ", constNumber) // const keyword not hosted
+// const constNumber = 100
 
-// console.log(x());  // Arrow fucntion not hoisting concept because arrow function are not hosted
-// var x = () => {
+// console.log("Arrow function called: ", arrowFunction);  // Arrow function not hoisting concept because arrow function are not hosted
+// var arrowFunction = () => {
 //     return 100;
 // }
 
-// 1. Arrow fucntion not hoisted
-// 2. this in arrow function is binded to the parent
-
-
-// host(100)
-// function host (number) {
-//     console.log("number is ", number)
+// console.log("Regular function called: ", regularFunction);
+// var regularFunction = function () {
+//     return 100;
 // }
 
-// const obj = {
+// regularFunction(100)
+// function regularFunction(number) {
+//     console.log("Print number: ", number)
+// }
+
+// var obj = {
 //     name: "Prakash",
 //     someMethod: () => {
 //         return this;
 //     }
 // }
 
-// var a = obj.someMethod();
-// console.log(this)
-// console.log("A value is empty object", a); // in arrow function this keyword binded(refer) to parant object
+// var extractSomeMethod = obj.someMethod();
+// console.log("Object.someMethod value is empty object, because called via obj references", extractSomeMethod);
 // console.log("Object.someMethod also got empty object", obj.someMethod());
 
 // function myFucntion() {
@@ -50,35 +54,35 @@
 //     }
 // }
 
-// const myObj =  new myFucntion();
+// const myObj = new myFucntion();
 // console.log(myObj)
 // console.log("myObj", new myObj())
 
 
-// const myFunction  = () => { // Arrow function does not have new keyword its not construct
+// var arrowFunction = () => { // Arrow function does not have new keyword its not construct
 //     return this;
 // }
-// const day = new myFunction();
-// console.log(day())
+// var arrowFun = new arrowFunction();
+// console.log("Called arrow function using new keyword: ", arrowFun())
 
-
-//  DIffernces between arrow function and normal fucntion
+/*Differences between arrow function and normal function*/
 // function myCar() {
-//     console.log("First print", this)
+//     console.log("First Print: ", this)
 //     this.fuel = 10;
 //     this.reFuel = function () {
-//         const fuel = this.fuel;
-//         console.log("Second Print", fuel)
-//         setTimeout(function () { // We cannot access this.fuel because when we use this keyword its refer to global object inside settimeout funciton nameless funtion called   without any execution context
-//             console.log("Third Print", fuel)
+//         var fuel = this.fuel;
+//         console.log("Second Print: ", fuel);
+//         setTimeout(function () {
+//             /*We cannot access this.fuel because when we use this keyword its refer to global object inside settimeout funciton nameless funtion called without any execution context*/
+
+//             console.log("Third Print: ", fuel)
 //             var fuels = fuel + 100
-//             console.log("fuel is", fuels)
+//             console.log("Fourth Print: ", fuels)
 //         }, 1000);
 //     }
 // }
-
-// const benz  = new myCar();
-// console.log(benz.reFuel())
+// const benz = new myCar();
+// console.log("Refuel function called: ", benz.reFuel())
 
 // function myCar() {
 //     console.log("First print", this)
@@ -93,69 +97,59 @@
 
 //     }
 // }
-// const benz  = new myCar();
-// benz.reFuel(); // in arrow function this refer to parant object
+// var benz = new myCar();
+// benz.reFuel(); // In Arrow function this refer to parent object context
 
-
-// const sta  = function (){
-//     console.log(this)
+// var thisFunc = function (){
+//     console.log("Global object: ", this);
 // }
-// sta();
+// thisFunc();
 
-// const arrow  = () => {
-//     console.log(this)
+// var arrow = () => {
+//     console.log("Print arrow function this value: ", this)
 // }
-// const arrowFun = arrow // anonymous and arrow function we cant use new keyword and also we cant call direclty
+// var arrowFun = arrow // Anonymous and arrow function we cant use new keyword and also we cant call directly
 // arrowFun()
-
 
 // Note
 // 1. Normal function both constructable and callable
-// 2. arrow function only callable
-
-
-
+// 2. Arrow function only callable
 
 // var sum = (a,b) => a + b; // Here we don't need return because single line without curly braces does not need return
-// console.log(sum(2,2))
+// console.log("Arrow function print: ", sum(2,2))
 
 // var sumOne = (a,b) => {
 //     return a + b // Here we need to return the value because we are using curly braces more then one line we need block
 // };
+// console.log("Using curly braces in arrow functions: ", sumOne(2,25));
 
-// console.log(sumOne(2,25))
-
-
-// function test() {
-//     this.name = "prakash";
+// function normalFunction() {
+//     this.name = "PRAKASH";
 //     setTimeout(function() {
-//         console.log('this',this.name) // We got undefined because its not refer to name its refer to the global object because  without execution context
+//         /*We got undefined because its not refer to name its refer to the global object because without execution context*/
+//         console.log('Print this.name but got undefined: ',this.name)
 //     }, 1000)
 // }
-// // test()
+// normalFunction()
 
-// function testone() {
-//     this.name = "prakash";
+// function normalFunction() {
+//     this.name = "PRAKASH";
 //     setTimeout(() => {
-//         console.log('this',this.name) // its refer to the global object because its nameless function without execution context
+//         console.log('Print this.name: ', this.name) // its refer to the global object because its nameless function without execution context
 //     }, 1000)
 // }
+// console.log("Directly regular function called: ", normalFunction());
+// console.log("Create new empty object and called: ", new normalFunction())
 
-// console.log(testone())
-// console.log(new testone())
+/*https://www.youtube.com/watch?v=dWZIPIc3szg Link*/
 
-
-
-// https://www.youtube.com/watch?v=dWZIPIc3szg Link
-
-// console.log(this)
+// console.log("Print this value", this)
 // var age  = 27;
 // function displayAge() {
-//     console.log(age)
-//     console.log(this)
+//     console.log("Print age value: ", age)
+//     console.log("Print this inside the function: ", this)
 // }
-
-// // displayAge(); // Whenever javascript excute the function internally window object added window.displayAge() this rerfer to the window object
+// displayAge(); // Whenever JAVASCRIPT execute the function internally window object added window.displayAge() this refer to the window object
 
 // var person = {
 //     age: 38,
@@ -168,7 +162,6 @@
 // // In javascript value of this keyword determined by how a function is called (runtime binding)
 // person.displayAge() // This keyword refer to the object that is currently calling function output 38
 // person.nestedPerson.displayAge() // This keyword refer to the object that is currently calling function output 40
-
 
 // //  THis is Normal function example
 // var personOne = {
